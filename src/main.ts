546be2b6fd33610dfdb04c321dea980424d6bce0
Bergmeier <joern.bergmeier@arvato-scs.com>
2021-11-19 23:24:57 +0100

    running basic server construct

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import logger from './utilities/logger';
+
+dotenv.config();
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+
+app.get("/", logger, (req, res) => {
+    console.log("hllo");
+    res.send("hello from APU")
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
+
+
 const myFunc = (num: number): number => {
     return num * num;
   };
